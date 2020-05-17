542d02f1a331ae8f2b6f70c068f36ec304d8a334
Oliver Ponder <oliver.ponder@gmail.com>
2020-05-17 13:27:26 +0800

    Add arrow key pagination through sentences of a story.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
-(async function () {
-  const storyText = document.getElementById("story-text");
-  const backgroundIframe = document.getElementById("background");
-  const storiesURL = '/stories.json';
-  const body = document.body;
+let currentStory = {
+  text: "",
+  page: "",
+  totalPages: ""
+};
 
+const storyText = document.getElementById("story-text");
+const counter = document.getElementById("counter");
+const backgroundIframe = document.getElementById("background");
+const storiesURL = '/stories.json';
+const body = document.body;
+
+const RIGHT_ARROW = 39;
+const LEFT_ARROW = 37;
+
+document.addEventListener("keydown", (event) => {
+  switch(event.which) {
+    case RIGHT_ARROW:
+      rightArrowPressed()
+      break;
+    case LEFT_ARROW:
+      leftArrowPressed()
+      break;
+    default:
+      // Unregistered key press, do nothing.
+  }
+});
+
+(async function () {
   try {
     const response = await fetch(storiesURL);
     if (response.status !== 200) {
@@ -13,11 +36,25 @@
 
     const randomStory = stories[Math.floor(Math.random() * stories.length)]
 
-    storyText.innerHTML = randomStory.text;
+    let sentences = randomStory.text.split(".");
+    sentences = sentences.map((x) => x.trim());
+    sentences = sentences.filter((x) => x)
+
+    currentStory = {
+      sentences: sentences,
+      sentence: 0,
+      sentenceCount: sentences.length
+    };
+
+    renderStory(currentStory);
 
     backgroundIframe.src = `/backgrounds/${randomStory.background}.html`
 
     body.classList.add("done");
+
+  setTimeout(() => {
+    storyText.classList.add("loaded");
+  }, 1000)
 
   } catch(error) {
     console.log(error);
@@ -25,3 +62,32 @@
     console.log("Done");
   }
 })()
+
+function renderStory(story) {
+  storyText.classList.add("start");
+  setTimeout(() => {
+    storyText.innerHTML = story.sentences[story.sentence] + "." ;
+    counter.innerHTML = (story.sentence +1) + "/" + story.sentenceCount;
+    storyText.classList.remove("start");
+  }, 100);
+}
+
+function rightArrowPressed() {
+  if (currentStory.sentence === currentStory.sentenceCount - 1) {
+    console.error("Can't go past the end of the story");
+    return;
+  }
+
+  currentStory.sentence += 1;
+  renderStory(currentStory);
+}
+
+function leftArrowPressed() {
+  if (currentStory.sentence === 0) {
+    console.error("Can't go further back");
+    return;
+  }
+
+  currentStory.sentence -= 1;
+  renderStory(currentStory);
+}
