1e407a8da60b043cc1a08b66314704b120a89008
Oliver Ponder <oliverponder@giantswarm.io>
2020-05-18 16:04:26 +0800

    Add arrow key pagination through sentences of a story. (#3)
    * Add arrow key pagination through sentences of a story.
    
    * Better on narrow screens and indicate you have reached the end by shaking the counter.
    
    * Add permalinks
    
    * Add button to get another random story. (#5)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
-(async function () {
-  const storyText = document.getElementById("story-text");
-  const backgroundIframe = document.getElementById("background");
-  const storiesURL = '/stories.json';
-  const body = document.body;
+let currentStory;
 
+const storyText = document.getElementById("story-text");
+const storyTitle = document.getElementById("story-title");
+const counter = document.getElementById("counter");
+const nextRandomButton = document.getElementById("next-random");
+const backgroundIframe = document.getElementById("background");
+const storiesURL = '/stories.json';
+const body = document.body;
+
+const RIGHT_ARROW = 39;
+const LEFT_ARROW = 37;
+
+document.addEventListener("keydown", (event) => {
+  switch(event.which) {
+    case RIGHT_ARROW:
+      rightArrowPressed()
+      break;
+    case LEFT_ARROW:
+      leftArrowPressed()
+      break;
+    default:
+      // Unregistered key press, do nothing.
+  }
+});
+
+(async function () {
   try {
     const response = await fetch(storiesURL);
     if (response.status !== 200) {
@@ -11,13 +32,39 @@
     }
     const stories = await response.json();
 
-    const randomStory = stories[Math.floor(Math.random() * stories.length)]
+    let story;
 
-    storyText.innerHTML = randomStory.text;
+    if (window.location.hash) {
+      let storyID = window.location.hash.substr(1);
+      story = stories.find((story) => story.permalink == storyID);
+    } else {
+      story = stories[Math.floor(Math.random() * stories.length)]
+    }
 
-    backgroundIframe.src = `/backgrounds/${randomStory.background}.html`
+
+
+    window.location.hash = story.permalink;
+
+    let sentences = story.text.split(".");
+    sentences = sentences.map((x) => x.trim());
+    sentences = sentences.filter((x) => x)
+
+    currentStory = {
+      sentences: sentences,
+      sentence: 0,
+      sentenceCount: sentences.length,
+      title: story.permalink
+    };
+
+    renderStory(currentStory);
+
+    backgroundIframe.src = `/backgrounds/${story.background}.html`
 
     body.classList.add("done");
+
+  setTimeout(() => {
+    storyText.classList.add("loaded");
+  }, 1000)
 
   } catch(error) {
     console.log(error);
@@ -25,3 +72,54 @@
     console.log("Done");
   }
 })()
+
+function renderStory(story) {
+  storyText.classList.add("start");
+  setTimeout(() => {
+    storyTitle.innerHTML = story.title ;
+    storyText.innerHTML = story.sentences[story.sentence] + "." ;
+    counter.innerHTML = (story.sentence +1) + "/" + story.sentenceCount;
+    storyText.classList.remove("start");
+  }, 100);
+}
+
+function rightArrowPressed() {
+  if (currentStory.sentence === currentStory.sentenceCount - 1) {
+    console.error("Can't go past the end of the story");
+    shakeCounter();
+    return;
+  }
+
+  if (currentStory.sentence === currentStory.sentenceCount - 2){
+    nextRandomButton.classList.add("lightup");
+  }
+
+  currentStory.sentence += 1;
+  renderStory(currentStory);
+}
+
+function leftArrowPressed() {
+  if (currentStory.sentence === 0) {
+    console.error("Can't go further back");
+    shakeCounter();
+    return;
+  }
+
+  if (currentStory.sentence < currentStory.sentenceCount ){
+    nextRandomButton.classList.remove("lightup");
+  }
+
+  currentStory.sentence -= 1;
+  renderStory(currentStory);
+}
+
+let shakeTimeout;
+
+function shakeCounter() {
+    counter.classList.add("shake");
+    clearTimeout(shakeTimeout);
+
+    shakeTimeout = setTimeout(() => {
+      counter.classList.remove("shake");
+    }, 200);
+}
